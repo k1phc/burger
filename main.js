8c8477fe1973379a1ab7b87c2a622065c410f15f
Kirill Sinyushkin <abigor_89@mail.ru>
2017-11-13 20:15:28 +0300

    fixed

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-var menuOpen = document.querySelector('#menuOpen');
-var menuClose = document.querySelector('#menuClose');
+var menuOpen = document.getElementById('menuOpen');
+var menuClose = document.getElementById('menuClose');
 var menuList = document.querySelector('.hamb-menu__list');
 
-
-menuOpen.addEventListener('click', function(elem){
+menuOpen.addEventListener('click', function(){
     menuList.classList.add('hamb-menu__list_active');
 });
 menuClose.addEventListener('click', function(){
